5968572fbc5ce7db5250820d1f34b907c2f371ea
Elisabetta Gabriele <elisabettag80@gmail.com>
2019-09-27 18:43:13 -0400

    Elisabettas change test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button>Click me here example</Button>
+        <Button>Click me here example elisabetta</Button>
         <Button>this is another button for testing purposes</Button>
         <h1>this will be our boiler plate code!</h1>
         <p>frontend team</p>
