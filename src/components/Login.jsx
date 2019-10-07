02491b03ab67d9be0c1d443e7e673c00eadfa8c3
chuyanchenxxx <cc7997a@student.american.edu>
2019-10-07 12:20:26 -0400

    Some changes (updates layout)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import { Button, Header, Label, Form, Checkbox, Divider, Segment, Grid } from 'semantic-ui-react';
+import { Button, Header, Label, Form, Checkbox, Divider, Segment, Grid, TextArea, Icon } from 'semantic-ui-react';
 
 /* the login/signup page. **/
 
@@ -13,77 +13,82 @@ const genderOptions = [
 export default class Login extends Component {  
     render(){
         return(
-            <Grid.Column width={15}>
-                <Segment fluid>
-                     <div>
-                        <header>Log in</header>
-                        <Form>
-                            <Form.Field>
-                                <Label>Username / E-mail</Label>
-                                <input placeholder='Username / E-mail' />
-                            </Form.Field>
-                            <Form.Field>
-                                <Label>Password</Label>
-                                <input placeholder='Password' />
-                            </Form.Field>
-                            <Form.Field>
-                                <Checkbox label = 'Keep me logged in' />
-                         </Form.Field>
-                            <Button type = 'Log in'>Log in</Button>
-                            <Button type = 'Cancel'>cancel</Button>
-                            <Header>Not having an account?</Header>
-                            <Button type = 'Signup'>Sign up</Button>
-                        </Form>
-                
-                        <Divider></Divider>
-                        <header>Sign up</header>
-                        <Form>
-                            <Form.Field>
-                                <label>Username</label>
-                                <input placeholder = 'Username' />
-                            </Form.Field>
-                            <Form.Field>
-                                <label>E-mail</label>
-                                <input fluid label = 'your E-mail address' placeholder = 'E-mail address' />
-                            </Form.Field>
-                            <Form.Field>
-                                <label>Re-enter E-mail</label>
-                                <input fluid label = 're-enter your E-mail address' placeholder = 'Re-enter E-mail' />
-                            </Form.Field>
-                            <Form.Group widths = 'equal'>
-                                <Form.input fluid label = 'First name' placeholder = 'First name' />
-                                <Form.input fluis label = 'M.' placeholder = 'M.' />
-                                <Form.input fluid label = 'Last name' placeholder = 'Last name' />
-                            </Form.Group>
-                            <Form.Select 
-                                fluid 
-                                label = 'Gender' 
-                                options = {genderOptions} 
-                                placeholder = 'Gender' 
-                            />
-                            <header> Birthdate </header>
-                            <Form.Group inline>
-                                <Form.input fluid label = 'MM' placeholder = 'Month'/>
-                                <Form.input fluid label = 'DD' placeholder = 'Day'/>
-                                <Form.input fluid label = 'YYYY' placeholder = 'Year'/>
-                            </Form.Group>
-                            <Form.Field>
-                                <label>Password</label>
-                                <input fluid label = 'Password (longer than 8 characters)' placeholder = 'Password' />
-                            </Form.Field>
-                            <Form.Field>
-                                <label>Re-enter assword</label>
-                                <input fluid label = 'Re-enter your password' placeholder = 'Re-enter password' />
-                            </Form.Field>
-                            <Form.Checkbox label="I hereby agree all terms and conditions..." />
-                            <Form.Group inline>
-                                <Form.Button>Signup</Form.Button>
-                                <Form.Button>Cancel</Form.Button>
-                            </Form.Group>
-                        </Form>
-                    </div>
-                </Segment>
-            </Grid.Column>
+            <Grid textAlign = 'center' style = {{height: '130vh'}} verticalAlign = 'middle'>
+                <Grid.Column style = {{maxWidth: 600}}>
+                    <Header as = 'h1' color = 'teal' textAlign = 'left'>SpectraSearch</Header>    
+                        <Segment stacked>
+                            <Form size = 'large'>
+                                <Header as = 'h2' color = 'teal' textAlign = 'left'>Log In</Header> 
+                                <Form.Field>
+                                    <Label textAlign = 'left'>Username / E-mail</Label>
+                                    <input fluid icon = 'user' iconPosition = 'left' placeholder='Username / E-mail' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Label>Password</Label>
+                                    <input placeholder='Password' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Checkbox label = 'Keep me logged in' />
+                                </Form.Field>
+                                <Button type = 'Log in' primary>Log in</Button>
+                                <Button type = 'Cancel'>cancel</Button>
+
+                                <Divider horizontal>Not having an account?</Divider>
+                                <Button type = 'Signup' primary >Sign up</Button>
+                            </Form>
+                        
+                            <Divider section/>
+                            <header size  = 'huge'>Sign up</header>
+                            <Form>
+                                <Form.Field>
+                                    <label>Username</label>
+                                    <input placeholder = 'Username' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <label>E-mail</label>
+                                    <input fluid label = 'your E-mail address' placeholder = 'E-mail address' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <label>Re-enter E-mail</label>
+                                    <input fluid label = 're-enter your E-mail address' placeholder = 'Re-enter E-mail' />
+                                </Form.Field>
+                                <Form.Group widths = 'equal'>
+                                    <Form.Field
+                                        id='First Name'
+                                        control={TextArea}
+                                        label='First Name'
+                                        placeholder='First Name'
+                                    />
+                                    <Form.Field
+                                        id='M.'
+                                        control={TextArea}
+                                        label='Middle'
+                                        placeholder='Middle'
+                                    />
+                                    <Form.Field
+                                        id='Last Name'
+                                        control={TextArea}
+                                        label='Last Name'
+                                        placeholder='Last Name'
+                                    />
+                                </Form.Group>
+
+                                <Form.Field>
+                                    <Checkbox label = 'I hereby agree to all conditions and terms.' />
+                                </Form.Field>
+
+                                <Form.Group widths = 'equal'>
+                                    <Form.Button primary>Signup</Form.Button>
+                                    <Form.Button>Cancel</Form.Button>
+                                </Form.Group>
+
+                            </Form>
+
+
+
+                    </Segment>
+                </Grid.Column>
+            </Grid>
         )   
     }
 }
