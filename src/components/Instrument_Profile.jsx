1d2f5b3d64538039e64efcf464a1edd8696ed7d0
RebeccaSak <Rebecca.sak@aol.com>
2019-10-07 11:00:42 -0400

    changed id to class

--- src/components/Instrument_Profile.jsx
+++ src/components/Instrument_Profile.jsx
@@ -20,18 +20,18 @@ export default class InstProf extends Component {
     render () {
         return (
             <Segment>
-                <div id="Heading Info">
+                <div class="Heading Info">
                     <Header size='huge'>LED Bulb</Header>
                     <Header size='medium'>Catalog #: 902398378</Header>  
                 </div>
 
-                <div id="Basic Info">
+                <div class="Basic Info">
                     <Header size='medium'>Basic Information</Header>
                     <div> Manufacturer: Lighting Co.</div>
                     <div> Description: This is a great lightbulb to use everyday. </div>
                 </div>
 
-                <div id="Creation Info">
+                <div class="Creation Info">
                     <Header size='medium'>Creation Information</Header>
                     <div> Document Creator: John Smith</div>
                     <div> Labratory: Lighting Lab LLC </div>
@@ -39,12 +39,12 @@ export default class InstProf extends Component {
                     <div> Report Number: 0283 </div> 
                 </div>
 
-                <div id="Image">
+                <div class="Image">
                     <Header size='medium'>Spectral Graph</Header>
                     export default ImageExampleImage
                 </div>
 
-                <div id="Spectral Info">
+                <div class="Spectral Info">
                     <Header size='medium'>Spectral Information</Header>
                     <div> Spectral Quantity: transmittance</div>
                     <div> Reflection Geometry: di:8 </div>
