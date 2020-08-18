da5be7781262737d7908c36239f75af4d0116771
Elisabetta Gabriele <elisabettag80@gmail.com>
2020-08-18 19:53:04 -0400

    dump data

--- public/filename.sql
+++ public/filename.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
--- Host: localhost    Database: dataDump.db
+-- Host: localhost    Database: dataDump
 -- ------------------------------------------------------
 -- Server version	8.0.18
 
